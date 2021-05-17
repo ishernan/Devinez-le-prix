7eae296631a683414342869624a0dcfbffd881b2
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 18:01:50 +0200

    control formulaire

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const error = document.querySelector('small');
 
 let nombreDepart = Math.floor((Math.random() * 99) + 1); 
 
- prix.addEventListener('keyup', ()=> {
-   if (isNaN(prix.value)) {
+prix.addEventListener('keyup', ()=> {
+  if (isNaN(prix.value)) {
       error.classList.remove("d-none"); 
-   } else {
+  } else {
      error.classList.add("d-none"); 
-   }
+  }
  });
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+  if (isNaN(prix.value) || prix.value == '') {
+    prix.classList.add('nonNombre')
+  } else {
+    prix.classList.add('isNombre')
+  }
+})

f1563d0ca69822319d4607e8efc38853b5cff958
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 23:17:08 +0200

    controle limite 100

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const numero      = document.querySelector('#prix');
 const form        = document.querySelector('#formulaire');
-const error       = document.querySelector('small');
+const error       = document.querySelector('#small1');
+const error2      = document.querySelector('#small2');
 const instruction = document.querySelector('#instructions');
 const rejouer     = document.querySelector('#rejouer');
+
 
 let nombreDepart = Math.floor((Math.random() * 99) + 1 ); 
 console.log(nombreDepart)
@@ -39,7 +41,13 @@ form.addEventListener('submit', (e) => {
   e.preventDefault(); 
   if (isNaN(numero.value) || numero.value == '') {
     numero.classList.add('nonNombre')
-  } else {
+  } else if (numero.value > 100) {
+    error2.classList.remove("d-none")
+    setTimeout(()=>{
+      error2.classList.add("d-none")
+    },2000);
+
+  } else {    
     coups ++;
     numero.classList.add('isNombre')
     nombreChoisi = numero.value; 

ec26ade1c8ea6eafa4993748b73fd30278b7bc4e
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 17:29:23 +0200

    control input

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const error = document.querySelector('small');
 
 let nombreDepart = Math.floor((Math.random() * 99) + 1); 
 
- 
+ prix.addEventListener('keyup', ()=> {
+   if (isNaN(prix.value)) {
+      error.classList.remove("d-none"); 
+   } else {
+     error.classList.add("d-none"); 
+   }
+ });
 

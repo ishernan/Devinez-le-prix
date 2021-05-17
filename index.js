371d5d5788d63b4da8754a6c4fde8d1c736dac39
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 19:13:57 +0200

    ajout de class

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ let nombreDepart = Math.floor((Math.random() * 99) + 1);
 let coups = 0; //les essais
 let nombreChoisi; //pas besoin que les variables aient une valeur
 
+function verifier(nombre) {
+  let information = document.createElement('div'); //pour creer un element HTML
+  if (nombre < nombreDepart) {
+    information.textContent = `#${coups} (${nombre}) C'est plus`;
+    information.className = 'instruction plus';
+    
+  } else if (nombre < nombreDepart) {
+    information.textContent = `#${coups} (${nombre}) C'est plus`;
+    information.className = 'instruction moins';
+  } else {
+    information.textContent = "#" + coups + " ( " + nombre + " ) Félicitations vous avez trouvé le juste prix !";
+    information.className = "instruction fini";
+    prix.disabled = true; //pour desactiver le form quand on trouve le numero 
+  }
+    document.querySelector('#instructions').prepend(information);
+}
+
 prix.addEventListener('keyup', ()=> {
   if (isNaN(prix.value)) {
       error.classList.remove("d-none"); 

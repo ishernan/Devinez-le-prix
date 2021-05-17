69e94bb913b1ba5e6559375e68a7181347c9a449
Isaac Hernandez <isaac.hernandez@outlook.com>
2021-05-17 16:54:28 +0200

    nombre aleatoire du depart

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const prix  = document.querySelector('#prix');
 const form  = document.querySelector('#formulaire');
 const error = document.querySelector('small');
 
+let nombreDepart = Math.floor((Math.random() * 99) + 1); 
+
+ 
 
